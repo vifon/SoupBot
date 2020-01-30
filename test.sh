3dae5049891a1bac3b6d188ad48d00c72988d26f
Wojciech Siewierski <wojciech@siewierski.eu>
2020-01-30 19:51:04 +0100

    Rework the test launcher; add test coverage checks

--- test.sh
+++ test.sh
@@ -1,10 +1,63 @@
 #!/bin/bash
 
-./test_server.py &
-sleep 0.2
+set -o errexit -o nounset -o pipefail
 
-if [ "$1" = "-v" ]; then
-    ./bot.py test_config.yml
-else
-    ./bot.py test_config.yml &> /dev/null
-fi
+VERBOSE=0
+COVERAGE=0
+TIMEOUT=0
+
+while getopts "vct" ARG; do
+    case "$ARG" in
+        v)
+            VERBOSE=1
+            ;;
+        c)
+            COVERAGE=1
+            ;;
+        t)
+            TIMEOUT=1
+            ;;
+        ?)
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
+
+start_test_server() {
+    ./test_server.py &
+    SERVER_PID="$!"
+    sleep 0.2
+}
+
+start_test_client() {
+    local COMMAND
+    COMMAND=(./bot.py test_config.yml)
+
+    if (( "$COVERAGE" )); then
+        COMMAND=(
+            coverage run --branch --source=bot,irc "${COMMAND[@]}"
+        )
+    fi
+
+    if (( "$TIMEOUT" )); then
+        COMMAND=(timeout 20s "${COMMAND[@]}")
+    fi
+
+    if (( "$VERBOSE" )); then
+        "${COMMAND[@]}"
+    else
+        "${COMMAND[@]}" &> /dev/null
+    fi
+}
+
+stop_test_server() {
+    kill "$SERVER_PID" 2> /dev/null && echo "Killed the test server." || true
+    exit
+}
+
+trap stop_test_server EXIT INT TERM
+start_test_server
+start_test_client "$@"
