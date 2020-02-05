3b1bfaa3e88d983b35ec7f0ae2399cbeee8468c2
Wojciech Siewierski <wojciech@siewierski.eu>
2020-02-05 21:33:51 +0100

    Migrate from aiohttp to httpx
    httpx seems more future-proof (pun not intended).

--- irc/plugins/http_preview.py
+++ irc/plugins/http_preview.py
@@ -3,13 +3,8 @@ from irc.plugin import IRCPlugin
 
 from bs4 import BeautifulSoup
 from urlextract import URLExtract
-import aiohttp
 import asyncio
-
-
-async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
+import httpx
 
 
 class HTTPPreview(IRCPlugin):
@@ -19,16 +14,18 @@ class HTTPPreview(IRCPlugin):
         if msg.command == 'PRIVMSG':
             channel = msg.args[0]
             nick = msg.sender.nick
-            for url in self.extractor.gen_urls(msg.body):
-                self.logger.info("Generating preview for: %s", url)
-                try:
-                    async with aiohttp.ClientSession() as session:
+            async with httpx.AsyncClient() as client:
+                for url in self.extractor.gen_urls(msg.body):
+                    self.logger.info("Generating preview for: %s", url)
+                    try:
                         try:
-                            html = await asyncio.wait_for(
-                                fetch(session, url),
+                            response = await asyncio.wait_for(
+                                client.get(url),
                                 timeout=self.config.get('timeout', 10),
                             )
                             self.logger.debug("%s fetched.", url)
+                            response.raise_for_status()
+                            html = response.text
                             loop = asyncio.get_event_loop()
                             soup = await asyncio.wait_for(
                                 loop.run_in_executor(
@@ -55,5 +52,7 @@ class HTTPPreview(IRCPlugin):
                                 'PRIVMSG', channel,
                                 body=f"{nick}: {title}",
                             ))
-                except Exception:
-                    self.logger.exception("Error during processing %s", url)
+                    except Exception:
+                        self.logger.exception(
+                            "Error during processing %s", url
+                        )
