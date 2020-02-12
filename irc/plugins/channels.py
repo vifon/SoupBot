335334853c826e230593e2bc3b51fa7e385b7033
Wojciech Siewierski <wojciech@siewierski.eu>
2020-02-12 01:30:55 +0100

    More strict type hints in ChannelManager

--- irc/plugins/channels.py
+++ irc/plugins/channels.py
@@ -1,12 +1,16 @@
 from irc.message import IRCMessage
 from irc.plugin import IRCPlugin
 
+from typing import Set
+
 
 class ChannelManager(IRCPlugin):
-    def start(self):
+    shared_data: Set[str]
+
+    def start(self) -> None:
         super().start()
 
-        channels = set(self.config['channels'])
+        channels: Set[str] = set(self.config['channels'])
         join = channels.difference(self.shared_data)
         part = self.shared_data.difference(channels)
         self.shared_data = channels
@@ -18,5 +22,5 @@ class ChannelManager(IRCPlugin):
             self.logger.info("Parting %s…", channel)
             self.client.send(IRCMessage('PART', channel))
 
-    def _shared_data_init(self):
+    def _shared_data_init(self) -> Set[str]:
         return set()
